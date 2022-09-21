3c15f14e695c7575460bebb2853e41e60e1316be
Manuel Cano Gil <mcangil786@g.educaand.es>
2022-09-21 13:16:57 +0200

    Proyecto de Java Gestion de Peajes

--- src/main/java/ITicket.java
+++ src/main/java/ITicket.java
@@ -1,6 +1,5 @@
 package main.java;
 
 public interface ITicket {
-    public void entradaAutopista();
     public float calculoTarifa(float kmRecorrido, float precio);
 }
--- src/main/java/Ticket.java
+++ src/main/java/Ticket.java
@@ -1,5 +1,12 @@
 package main.java;
 
-public abstract class Ticket {
-    
+public abstract class Ticket implements ITicket {
+    private float kmEntrada;
+    private float kmSalida;
+    private float tarifa=0.00f;
+
+    public Ticket(float kmEntrada, float tarifa) {
+        this.kmEntrada = kmEntrada;
+        this.tarifa = tarifa;
+    }
 }
